f7695e0ff4a4732b2205eb7d419e5a1f1fd0248d
Elefrea <elefrea@hotmail.com>
2021-03-19 10:08:49 +0000

    sonde2 root usage added

--- sonde2_cpu.sh
+++ sonde2_cpu.sh
@@ -4,6 +4,5 @@
 for user in $(who | sed 's/ .*//' | sort -u)
 do 
 	(top -b -n 1 -u $user | awk -v user=$user 'NR>7 {sum += $9;} END {print user, sum;}')
-	sleep 0.05
 done
-wait
+(top -b -n 1 -u "root" | awk -v user="root" 'NR>7 {sum += $9;} END {print user, sum;}')
