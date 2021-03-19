1da2029242f75482edbdd25e2b1ea7e3a7a433c3
Elefrea <elefrea@hotmail.com>
2021-03-19 10:12:31 +0000

    sonde3 beginning

---
+++ sonde3_mem.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+for user in $(who | sed 's/ .*//' | sort -u)
+do 
+	(top -b -n 1 -u $user | awk -v user=$user 'NR>8 {sum += $9;} END {print user, sum;}')
+done
+(top -b -n 1 -u "root" | awk -v user="root" 'NR>8 {sum += $9;} END {print user, sum;}')
