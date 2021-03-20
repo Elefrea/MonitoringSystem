5e0f1f5310781326d1525b6ca0d4c537dbab7f3b
Elefrea <elefrea@hotmail.com>
2021-03-20 19:25:10 +0000

    db work started

--- sonde2_cpu.sh
+++ sonde2_cpu.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-
+file=/home/green/MonitoringSys/MonitoringSystem/monitoring.db
+sqlite3 $file <<EOF
+CREATE TABLE mem(date_time date NOT NULL, user text NOT NULL, mem_usage integer);
+CREATE TABLE cpu(date_time date NOT NULL, user text NOT NULL, cpu_usage integer);
+EOF
+sum=0
 for user in $(who | sed 's/ .*//' | sort -u)
 do 
-	(top -b -n 1 -u $user | awk -v user=$user 'NR>7 {sum += $9;} END {print user, sum;}')
+	(top -b -n 1 -u $user | awk -v user=$user 'NR>7 {sum += $9;} END {print user, $sum=sum}')
+       	sqlite3 $file "INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), '$user', '$sum');"
 done
-(top -b -n 1 -u "root" | awk -v user="root" 'NR>7 {sum += $9;} END {print user, sum;}')
+(top -b -n 1 -u "root" | awk -v user="root" 'NR>7 {sum += $9;} END {print user, $sum=sum;}')
+ sqlite3 $file "INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), 'root', '$sum');"
+
