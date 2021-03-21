2c77aecf04b572bc40b6fd1e70ea5116452e5a8e
Elefrea <elefrea@hotmail.com>
2021-03-21 16:39:04 +0000

    sum reccupéré dans db, bugg database lock

--- sonde2_cpu.sh
+++ sonde2_cpu.sh
@@ -2,15 +2,16 @@
 
 file=/home/green/MonitoringSys/MonitoringSystem/monitoring.db
 sqlite3 $file <<EOF
+.timeout 3000
 CREATE TABLE mem(date_time date NOT NULL, user text NOT NULL, mem_usage integer);
 CREATE TABLE cpu(date_time date NOT NULL, user text NOT NULL, cpu_usage integer);
 EOF
-sum=0
+
 for user in $(who | sed 's/ .*//' | sort -u)
 do 
-	(top -b -n 1 -u $user | awk -v user=$user 'NR>7 {sum += $9;} END {print user, $sum=sum}')
+	sum=$(top -b -n 1 -u $user | awk 'NR>7 {sum += $9;} END {print sum}')
        	sqlite3 $file "INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), '$user', '$sum');"
 done
-(top -b -n 1 -u "root" | awk -v user="root" 'NR>7 {sum += $9;} END {print user, $sum=sum;}')
- sqlite3 $file "INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), 'root', '$sum');"
+sum=$(top -b -n 1 -u "root" | awk 'NR>7 {sum += $9;} END {print sum;}')
+sqlite3 $file "INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), 'root', '$sum');"
 
--- sonde3_mem.sh
+++ sonde3_mem.sh
@@ -2,14 +2,17 @@
 
 file=/home/green/MonitoringSys/MonitoringSystem/monitoring.db
 sqlite3 $file <<EOF
+.timeout 3000
 CREATE TABLE mem(date_time date NOT NULL, user text NOT NULL, mem_usage integer);
 CREATE TABLE cpu(date_time date NOT NULL, user text NOT NULL, cpu_usage integer);
 EOF
-sum=0
 
 for user in $(who | sed 's/ .*//' | sort -u)
 do 
-	(top -b -n 1 -u $user | awk -v user=$user 'NR>7 {sum += $10;} END {print user, $sum=sum;}')
+	
+	sum=$(top -b -n 1 -u $user | awk 'NR>7 {sum += $10;} END {print sum;}')
        	sqlite3 $file "INSERT INTO mem (date_time, user, mem_usage) values (datetime(), '$user', '$sum');"
 done
-(top -b -n 1 -u "root" | awk -v user="root" 'NR>7 {sum += $10;} END {print user, sum;}')
+sum=$(top -b -n 1 -u "root" | awk 'NR>7 {sum += $10;} END {print sum;}')
+sqlite3 $file "INSERT INTO mem (date_time, user, mem_usage) values (datetime(), 'root', '$sum');"
+
