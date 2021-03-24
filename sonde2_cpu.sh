445258bd8f8a9b17c135ee777be1e132f9585c2c
Elefrea <elefrea@hotmail.com>
2021-03-24 06:39:02 +0000

    yes

--- sonde2_cpu.sh
+++ sonde2_cpu.sh
@@ -2,7 +2,6 @@
 
 file=/home/green/MonitoringSys/MonitoringSystem/monitoring.db
 sqlite3 $file <<EOF
-.timeout 3000
 CREATE TABLE mem(date_time date NOT NULL, user text NOT NULL, mem_usage integer);
 CREATE TABLE cpu(date_time date NOT NULL, user text NOT NULL, cpu_usage integer);
 EOF
@@ -10,8 +9,8 @@ EOF
 for user in $(who | sed 's/ .*//' | sort -u)
 do 
 	sum=$(top -b -n 1 -u $user | awk 'NR>7 {sum += $9;} END {print sum}')
-       	sqlite3 $file "INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), '$user', '$sum');"
+       	sqlite3 $file "PRAGMA busy_timeout=3000; INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), '$user', '$sum');"
 done
 sum=$(top -b -n 1 -u "root" | awk 'NR>7 {sum += $9;} END {print sum;}')
-sqlite3 $file "INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), 'root', '$sum');"
+sqlite3 $file "PRAGMA busy_timeout=3000; INSERT INTO cpu (date_time, user, cpu_usage) values (datetime(), 'root', '$sum');"
 
--- sonde3_mem.sh
+++ sonde3_mem.sh
@@ -2,7 +2,6 @@
 
 file=/home/green/MonitoringSys/MonitoringSystem/monitoring.db
 sqlite3 $file <<EOF
-.timeout 3000
 CREATE TABLE mem(date_time date NOT NULL, user text NOT NULL, mem_usage integer);
 CREATE TABLE cpu(date_time date NOT NULL, user text NOT NULL, cpu_usage integer);
 EOF
@@ -11,8 +10,8 @@ for user in $(who | sed 's/ .*//' | sort -u)
 do 
 	
 	sum=$(top -b -n 1 -u $user | awk 'NR>7 {sum += $10;} END {print sum;}')
-       	sqlite3 $file "INSERT INTO mem (date_time, user, mem_usage) values (datetime(), '$user', '$sum');"
+       	sqlite3 $file "PRAGMA busy_timeout=3000; INSERT INTO mem (date_time, user, mem_usage) values (datetime(), '$user', '$sum');"
 done
 sum=$(top -b -n 1 -u "root" | awk 'NR>7 {sum += $10;} END {print sum;}')
-sqlite3 $file "INSERT INTO mem (date_time, user, mem_usage) values (datetime(), 'root', '$sum');"
+sqlite3 $file "PRAGMA busy_timeout=3000; INSERT INTO mem (date_time, user, mem_usage) values (datetime(), 'root', '$sum');"
 
