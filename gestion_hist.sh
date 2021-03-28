4cecc1d4200f01e332670117ad005b45de6748cc
green <green@kotmani>
2021-03-28 12:12:21 +0000

    chart fix

--- gestion_hist.sh
+++ gestion_hist.sh
@@ -8,7 +8,7 @@ DELETE FROM mem WHERE date_time < DATETIME('NOW', '-5 minutes');
 DELETE FROM users WHERE date < DATETIME('NOW', '-5 minutes');
 EOF
 
-echo `python3 /home/green/MonitoringSystem/visualization.py`
 echo `cp /home/green/MonitoringSystem/monitoring.db /home/green/MonitoringSystem/Backup/$(date +\%Y-\%m-\%d_\%H:\%M:\%S).db`
 echo `/usr/bin/find /home/green/MonitoringSystem/Backup/ -name "*.db" -mmin +1710 -exec rm -f {} \;`
+echo `python3 /home/green/MonitoringSystem/visualization.py`
 
