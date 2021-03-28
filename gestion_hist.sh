18747b734c1cd5bf8bffd80ec39f5e9638f572b3
green <green@kotmani>
2021-03-28 09:46:24 +0000

    visu testing

--- gestion_hist.sh
+++ gestion_hist.sh
@@ -2,6 +2,7 @@
 
 file=/home/green/MonitoringSys/MonitoringSystem/monitoring.db
 sqlite3 $file << EOF
+PRAGMA busy_timeout=3000;
 DELETE FROM cpu WHERE date_time < DATETIME('NOW', '-5 minutes');
 DELETE FROM mem WHERE date_time < DATETIME('NOW', '-5 minutes');
 DELETE FROM users WHERE date < DATETIME('NOW', '-5 minutes');
