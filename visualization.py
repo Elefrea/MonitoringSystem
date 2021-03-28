cf28371843cee9b85447121a4470ebae9982baf9
green <green@kotmani>
2021-03-28 11:17:48 +0000

    chart tests

--- visualization.py
+++ visualization.py
@@ -12,19 +12,27 @@ def main():
 
 def create_chart(table, cur):
     chart_val = []
-    if(table == "cpu"):
-        values = cur.execute("SELECT cpu_usage FROM cpu")
-    if(table == "mem"):
-        values = cur.execute("SELECT mem_usage FROM mem")
+    chart_date = []
+    users = cur.execute("SELECT user FROM users")
 
-    for value in values:
-        chart_val.append(value[0])
+    for user in set(users):
+        print(user[0])
+        green = "green"
+        if(table == "cpu"):
+            values = cur.execute("SELECT date_time, cpu_usage FROM cpu WHERE user=$1", (user[0],))
+        if(table == "mem"):
+            values = cur.execute("SELECT date_time, mem_usage FROM mem WHERE user=$1", (user[0],))
 
-    print(chart_val)
-    ofile = table + "_chart.svg"
-    bar_chart = pygal.Bar()
-    bar_chart.add(table, chart_val)
-    bar_chart.render_to_file(ofile)
+        for value in values:
+            chart_date.append(value[0])
+            chart_val.append(value[1])
+
+        print(chart_val)
+        ofile = table + "_chart.svg"
+        bar_chart = pygal.Bar()
+        bar_chart.x_labels = map(str, chart_date)
+        bar_chart.add(user[0]+" "+table+" usage", chart_val)
+        bar_chart.render_to_file(ofile)
 
 def create_connection(db_file):
     return sqlite3.connect(db_file)
