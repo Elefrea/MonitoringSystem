4135a46414364d0d03637b11dbaa49782cc49a62
green <green@kotmani>
2021-03-29 15:17:58 +0000

    sauvegarde crontab added

--- gestion_hist.sh
+++ gestion_hist.sh
@@ -9,6 +9,6 @@ DELETE FROM users WHERE date < DATETIME('NOW', '-5 minutes');
 EOF
 
 echo `cp /home/green/MonitoringSystem/monitoring.db /home/green/MonitoringSystem/Backup/$(date +\%Y-\%m-\%d_\%H:\%M:\%S).db`
-echo `/usr/bin/find /home/green/MonitoringSystem/Backup/ -name "*.db" -mmin +1710 -exec rm -f {} \;`
+echo `/usr/bin/find /home/green/MonitoringSystem/Backup/ -name "*.db" -mmin +10 -exec rm -f {} \;`
 echo `python3 /home/green/MonitoringSystem/visualization.py`
 
