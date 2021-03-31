7d3e6063dee8661534634af12b8fa721451f4c45
elefrea <elefrea@elefrea>
2021-03-31 07:29:23 +0000

    chart lagg fixed

--- visualization.py
+++ visualization.py
@@ -23,9 +23,7 @@ def create_chart(table, cur):
 
         for value in values:
             chart_date.append(value[0])
-            print(value[1])
-            print(type(value[1]))
-            chart_val.append(1)
+            chart_val.append(value[1])
 
         ofile = "/home/elefrea/Uni/MonitoringSystem/Visu/" + user[0] + "_" + table + "_chart.svg"
         line_chart = pygal.Line()
