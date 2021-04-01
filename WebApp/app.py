6de35dc199685a4df0b9ad229e2385ac8282ea8b
elefrea <elefrea@elefrea>
2021-04-01 07:45:58 +0000

    multiple images display

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, redirect, url_for, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    imageList = os.listdir('static')
+    imageList = ['/' + image for image in imageList]
+    return render_template('index.html', imageList=imageList)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
